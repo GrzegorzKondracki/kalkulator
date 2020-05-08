3e2b03843ef831f8774ea018388117c14f08347d
GrzegorzKondracki <g.kondracki@outlook.com>
2020-05-08 23:51:07 +0200

    js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const digits = document.getElementsByClassName('digit');
+let result = document.getElementsByClassName('result');
+
+let index = '8';
+let value = '';
+
+value = digits[index].textContent;
+
+// result.appendChild(value);
+
+result.textContent = 2;
