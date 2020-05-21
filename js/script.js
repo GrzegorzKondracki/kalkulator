0fcdd81071cecb5624a6e099e855ff73b8d12ae7
GrzegorzKondracki <g.kondracki@outlook.com>
2020-05-21 23:56:40 +0200

    script edited

--- js/script.js
+++ js/script.js
@@ -1,11 +1,28 @@
 const digits = document.getElementsByClassName('digit');
 let result = document.getElementsByClassName('result');
 
-let index = '8';
-let value = '';
+var showResult = function () {
 
-value = digits[index].textContent;
+}
+
+
+
+
+document.addEventListener('click', takeDigit);
+
+
+
+
+
+
+
+
+
+// let index = '8';
+// let value = '';
+
+// value = digits[index].textContent;
 
 // result.appendChild(value);
 
-result.textContent = 2;
+// result.textContent = 2;
